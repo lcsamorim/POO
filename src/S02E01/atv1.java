8a72885eea58904f0fecddf7744021090a9248b3
unknown <lcsamorim503@gmail.com>
2019-08-18 10:50:17 -0300

    commit 3

---
+++ src/S02E01/atv1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S02E01;
+
+/**
+ *
+ * @author Lucas Amorim
+ */
+public class atv1 {
+    public static void main (String args[]){
+    /*----------1-------------
+        for (int i=150;i<=300;i++){
+            System.out.print(i+" "); 
+                
+        }*/
+        
+    /*----------2-------------
+    int aux=0,i;
+    for (i=1;i<=1000;i++){
+        aux+=i;
+    }
+    System.out.println(aux);
+    */
+    
+    /*----------3-------------
+    for (int i=1;i<=100;i++){
+    if(i%3==0){
+        System.out.println(i);
+    }
+    }*/
+    int base=1;
+    for (int i=1;i<=10;i++){
+        System.out.println(base*=i);
+    }
+        
+    }
+}
